9b6dffdb2e2471bb226005c2edb4767675cff39f
Vale <ochoa.bedoya.valeria@gmail.com>
2025-08-21 09:22:28 -0500

    Creación de clases

--- consoleApp/Program.cs
+++ consoleApp/Program.cs
@@ -1,2 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Security.Cryptography.X509Certificates;
+
 Console.WriteLine("Hello, World!");
+
+public class Categorias
+{
+    public int id;
+    public string? nombre;
+}
+public class Musicos
+{
+    public int id;
+    public string? nombre;
+    public decimal reproducciones;
+    public bool grammy;
+    public DateTime fechaSalida;
+    public Categorias? categoria;
+
+}
